c0a85f22abbeac7a0a2cb417a30dd06f757f3260
sunnn <jss@gmail.com>
2024-06-29 14:28:33 -0700

    Your commit message here

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,37 @@
 /** @type {import('next').NextConfig} */
 
-const nextcors = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-};
+// const nextcors = {
+//   async headers() {
+//     return [
+//       {
+//         // matching all API routes
+//         source: "/api/:path*",
+//         headers: [
+//           { key: "Access-Control-Allow-Credentials", value: "true" },
+//           { key: "Access-Control-Allow-Origin", value: "*" },
+//           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+//           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+//         ]
+//       }
+//     ]
+//   }
+// };
 
-// const nextConfig = {
-//     reactStrictMode: true,
-//     images: {
-//         remotePatterns: [
-//             {
-//               protocol: 'https',
-//               hostname: 'tailwindui.com',
-//               port: '',
-//               pathname: '**',
-//             },
-//           ],
-//           domains: ['tailwindui.com','images.unsplash.com'],
-//           dangerouslyAllowSVG: true,
-//     },
-//   };
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'tailwindui.com',
+              port: '',
+              pathname: '**',
+            },
+          ],
+          domains: ['tailwindui.com','images.unsplash.com'],
+          dangerouslyAllowSVG: true,
+    },
+  };
   
-  export default nextcors
+  export default nextConfig
   
