4b135d26b91f73e2da317e66e6a1d2d972710ba8
sunnn <jss@gmail.com>
2024-07-01 14:49:05 -0700

    Your commit message

--- src/app/api/user/profile/[id]/route.ts
+++ src/app/api/user/profile/[id]/route.ts
@@ -58,7 +58,7 @@ export async function PATCH(req:Authrequest,{params}:{params:{id:string}}):Promi
 
                 if(oldimage){
 
-                    const oldimagepath = pathdata.join(process.cwd(), `/public/user/tmp/${oldimage}`);
+                    const oldimagepath = pathdata.join(process.cwd(), `tmp/public/user/${oldimage}`);
                     console.log('pathssss', oldimagepath);
                 
                     if (fs.existsSync(oldimagepath)) {
